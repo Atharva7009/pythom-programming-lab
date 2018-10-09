a72f8f4d00fa28d8968ed598617ff6aa8e66034a
Atharva7009 <43982769+Atharva7009@users.noreply.github.com>
2018-10-09 13:59:56 +0530

    Update lab_function.py

--- lab_function.py
+++ lab_function.py
@@ -1,14 +1,14 @@
-def armn(x):
- sum=0
- t=x
+def armn(x):                        #function declaration
+ sum=0          
+ t=x            
 
- while(t>0):
-  d=t%10
-  sum+=d**3
-  t=t//10
- if sum==x:
+ while(t>0):                        #while loop starts
+  d=t%10                            #we get the last number
+  sum+=d**3                         #cube of above value is added to sum 
+  t=t//10                           #removes remaining digits
+ if sum==x:                         #value of sum is matched with x if its matched then it prints if satatement  
    return'armstrong number'
  else:
-   return'not an armstrong no'
-x=int(input())
-print(armn(x))
+   return'not an armstrong no'      #if the values doesnt match this statement is executed
+x=int(input())                      #takes input from user
+print(armn(x))                      #calls functions
