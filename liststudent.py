6a7f660414f0ef01c63700a89c6a6939e740d84b
Atharva7009 <43982769+Atharva7009@users.noreply.github.com>
2018-11-27 12:57:57 +0530

    Add files via upload

---
+++ liststudent.py
@@ -0,0 +1,6 @@
+for i in range(3):
+	name=input('enter name of student:')
+	rollno=int(input('enter rollno of student:'))
+	marks=int(input('enter marks of student:'))
+	d=[name,rollno,marks]
+	print(d) 
